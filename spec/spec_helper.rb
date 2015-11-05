5d2a5f69cbee66b52fcfb42c071379b16066c246
yuemori <yuemori@aiming-inc.com>
2015-11-05 23:56:08 +0900

    Added simplecov print to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'slack_bot'
-
+require 'simplecov'
 require 'coveralls'
+
 Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start do
+  add_filter '.bundle/'
+end
