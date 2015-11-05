d9d942404945d48134f6e726d1829d5c5e2045f3
wakaba260 <moonoverwalker@gmail.com>
2015-11-05 22:55:23 +0900

    Added standard functions

---
+++ spec/slack_bot/event_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe SlackBot::Event do
+  let(:event) { SlackBot::Event.new(data) }
+  let(:data) do
+    {
+      type:     'message',
+      channel:  'C0D1TQL0L',
+      user:     'U04RNAV76',
+      text:     'this is test message',
+      ts:       '1446695868.000046',
+      event_ts: '1446695868.000046',
+      team:     'T029DFCEP'
+    }
+  end
+
+  describe 'defined_method' do
+    shared_examples_for 'check defined_method' do
+      describe 'has method' do
+        subject { event.respond_to? key }
+        it 'should be respond' do
+          is_expected.to be_truthy
+        end
+      end
+
+      describe 'return value' do
+        subject { event.send key }
+        it 'should be correct' do
+          is_expected.to eq return_value
+        end
+      end
+
+      describe ''
+    end
+
+    context 'type' do
+      let(:key) { :type }
+      let(:return_value) { data[:type] }
+      it_behaves_like 'check defined_method'
+    end
+
+    context 'text' do
+      let(:key) { :text }
+      let(:return_value) { data[:text] }
+      it_behaves_like 'check defined_method'
+    end
+
+    context 'when text is nil' do
+      subject { event.text }
+      let(:event) { SlackBot::Event.new(data_text_nil) }
+      let(:data_text_nil) { data.reject { |k, _v| k == :text } }
+
+      it { expect { subject }.not_to raise_error }
+      it 'should return empty return value' do
+        is_expected.to eq ''
+      end
+    end
+  end
+end
---
+++ spec/slack_bot/listener_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe SlackBot::Listener do
+  let(:listener) { SlackBot::Listener.new(pattern, callback) }
+  let(:pattern)  { /test/ }
+  let(:callback) { proc { result } }
+  let(:result)   { 'test' }
+  let(:event)    { SlackBot::Event.new(data) }
+  let(:data)     { { text: 'test' } }
+
+  describe '#call' do
+    context 'when match pattern' do
+      subject { listener.call(event) }
+
+      it 'should be execute' do
+        is_expected.to eq result
+      end
+    end
+
+    context 'when not match pattern' do
+      subject { listener.call(event) }
+      let(:data) { { text: 'hoge' } }
+
+      it 'should not be execute' do
+        is_expected.to eq nil
+      end
+    end
+  end
+end
