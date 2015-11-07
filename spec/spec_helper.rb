eee8a20b75b2a7d0d5eb993b2b1e1a8246c6318b
wakaba260 <moonoverwalker@gmail.com>
2015-11-07 19:45:04 +0900

    Removed gem dotenv

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,9 @@
 require 'slappy'
-require 'dotenv'
 require 'simplecov'
 require 'codeclimate-test-reporter'
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   CodeClimate::TestReporter::Formatter
 ]
 SimpleCov.start
-
-Dotenv.load
