20e8a3a59b449195d88fc28802559796b1fd90cc
wakaba260 <moonoverwalker@gmail.com>
2015-11-09 03:49:51 +0900

    Fix coverage settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
-require 'slappy'
 require 'simplecov'
 require 'codeclimate-test-reporter'
-
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   CodeClimate::TestReporter::Formatter
 ]
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require 'slappy'
