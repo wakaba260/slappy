d111b4e6ea5a51f7c12ddf2a8eb6949a05ae3a7d
wakaba260 <moonoverwalker@gmail.com>
2015-11-12 00:16:35 +0900

    Fixed not need double_quote to single_quote

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,8 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   CodeClimate::TestReporter::Formatter
 ]
 SimpleCov.start do
-  add_filter "/spec/"
+  add_filter '/spec/'
 end
 
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'slappy'
