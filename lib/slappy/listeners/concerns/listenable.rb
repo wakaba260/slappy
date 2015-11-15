bc435278cd314f65c989a6b8c20c65b0cf76faa0
wakaba260 <moonoverwalker@gmail.com>
2015-11-15 17:01:47 +0900

    Fixed raise NoMethodError when target_element is nil in Listenable#call

--- lib/slappy/listeners/concerns/listenable.rb
+++ lib/slappy/listeners/concerns/listenable.rb
@@ -12,14 +12,24 @@ module Slappy
 
       def call(event)
         return unless time_valid?(event)
-        target_element = self.class.name.split('::').last.gsub(/Listener$/, '').underscore.to_sym
-        event.matches = event.send(target_element).match pattern
+
+        target = event.send(target_element)
+        return unless target
+
+        event.matches = target.match pattern
         return unless event.matches
+
         @callback.call(event)
       end
 
       def time_valid?(event)
         Time.at(event.ts.to_f) > Slappy.client.start_time
+      end
+
+      private
+
+      def target_element
+        self.class.name.split('::').last.gsub(/Listener$/, '').underscore.to_sym
       end
     end
   end
