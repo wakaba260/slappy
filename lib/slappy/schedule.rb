4fc5a329c3c3dff9a0b3a4a54308db60228c78ec
wakaba260 <moonoverwalker@gmail.com>
2015-11-18 23:54:14 +0900

    Added debug logger to Schedule

--- lib/slappy/schedule.rb
+++ lib/slappy/schedule.rb
@@ -2,6 +2,8 @@ require 'securerandom'
 
 module Slappy
   class Schedule
+    include Slappy::Debuggable
+
     class OverScheduleError < StandardError; end
     class InvalidFormatError < StandardError; end
 
@@ -9,8 +11,15 @@ module Slappy
 
     def register(schedule, options = {}, &block)
       id = options[:id] || generate_id
-      schedule_list[id] = Thread.new { Chrono::Trigger.new(schedule) { block.call }.run }
-      Slappy.logger.try(:debug, "Schedule registerd to #{schedule}")
+      schedule_list[id] = Thread.new do
+        time = Chrono::Iterator.new(schedule).next
+        Debug.log "Schedule #{id} registerd to #{schedule}, first call to #{time}"
+        Chrono::Trigger.new(schedule) do
+          block.call
+          time = Chrono::Iterator.new(schedule).next
+          Debug.log "Schedule #{id} called by #{schedule}, next call to #{time}"
+        end.run
+      end
       id
     end
 
@@ -19,6 +28,7 @@ module Slappy
       if registered
         @schedule_list[id].kill
         @schedule_list.delete id
+        Debug.log "Schedule #{id} deleted"
       end
       registered
     end
