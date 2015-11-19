22a6e6460a04e7d0b3621a4db4727c7cdd95edc2
wakaba260 <moonoverwalker@gmail.com>
2015-11-20 02:40:00 +0900

    Modify Logger to be nil in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,9 @@ SimpleCov.start do
   add_filter '/spec/'
 end
 
+RSpec.configure do |config|
+  config.before(:all) { Slappy.configure { |c| c.logger = Logger.new(nil) } }
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'slappy'
