894262967950427c8c0595fe610ed54b5818d3ce
wakaba260 <moonoverwalker@gmail.com>
2015-11-20 01:13:40 +0900

    Modify Debuggable spec

--- spec/slappy/concerns/debuggable_spec.rb
+++ spec/slappy/concerns/debuggable_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe Slappy::Debuggable do
-  class Test
+  class Test1
     include Slappy::Debuggable
   end
 
-  let(:test_class) { Test.new }
+  class Test2
+    include Slappy::Debuggable
+  end
+
+  let(:test_class1) { Test1.new }
+  let(:test_class2) { Test2.new }
 
   describe 'Debug.log' do
     before do
@@ -15,11 +20,11 @@ describe Slappy::Debuggable do
         end
       end
     end
-    subject { Test::Debug.log message }
-
     let(:message) { 'message test' }
-    let(:result) { "[#{test_class.class.name}] #{message}\n" }
+    let(:result1) { "[#{test_class1.class.name}] #{message}\n" }
+    let(:result2) { "[#{test_class2.class.name}] #{message}\n" }
 
-    it { expect { subject }.to output(result).to_stdout }
+    it { expect { Test1::Debug.log message }.to output(result1).to_stdout }
+    it { expect { Test2::Debug.log message }.to output(result2).to_stdout }
   end
 end
