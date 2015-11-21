7b20defb9bf37cffa46c5906495826a28eadb90a
wakaba260 <moonoverwalker@gmail.com>
2015-11-22 06:20:03 +0900

    Modify SlackAPI.monitor_events

--- lib/slappy/slack_api/channel.rb
+++ lib/slappy/slack_api/channel.rb
@@ -1,7 +1,8 @@
 module Slappy
   module SlackAPI
     class Channel < Base
-      self.monitor_event = 'channel_created'
+      self.monitor_event =
+        %w(channel_created channel_deleted channel_rename channel_archive channel_unarchieve)
 
       def name
         '#' + @data.name
