428760e2cebadf847d19193f856bcd1c02eff4cb
wakaba260 <moonoverwalker@gmail.com>
2015-11-22 00:53:12 +0900

    Enable to from option

--- lib/slappy/listeners/base.rb
+++ lib/slappy/listeners/base.rb
@@ -9,13 +9,18 @@ module Slappy
 
       def initialize(pattern, options = {}, &callback)
         self.pattern = pattern
-        @options = options
+        if options[:from]
+          target.channel = options[:from][:channel]
+          target.user = options[:from][:user]
+        end
         @callback = callback
       end
 
       def call(event)
         Debug.log "Listen event call: #{target_element}:#{event.send(target_element)}"
+
         return unless valid?(event)
+        return unless target?(event)
 
         Debug.log "Callback event call: #{pattern}"
         @callback.call(event)
