0f4448951fd2abd58f84e733aba27b3c8f31b58c
wakaba260 <moonoverwalker@gmail.com>
2015-11-22 00:42:39 +0900

    Rename Validatable from Listenable module

--- lib/slappy/listeners/base.rb
+++ lib/slappy/listeners/base.rb
@@ -1,7 +1,23 @@
+require 'slappy/listeners/concerns/validatable'
+
 module Slappy
   module Listener
     class Base
-      include Listenable
+      include Validatable
+
+      def initialize(pattern, options = {}, &callback)
+        self.pattern = pattern
+        @options = options
+        @callback = callback
+      end
+
+      def call(event)
+        Debug.log "Listen event call: #{target_element}:#{event.send(target_element)}"
+        return unless valid?(event)
+
+        Debug.log "Callback event call: #{pattern}"
+        @callback.call(event)
+      end
     end
   end
 end
--- lib/slappy/listeners/concerns/validatable.rb
+++ lib/slappy/listeners/concerns/validatable.rb
@@ -1,46 +1,38 @@
 module Slappy
   module Listener
-    module Listenable
-      include ActiveSupport::Concern
+    module Validatable
       include Slappy::Debuggable
 
       attr_reader :pattern
 
-      def initialize(target, options = {}, &callback)
-        @pattern = target
-        @options = options
-        @callback = callback
-      end
-
-      def call(event)
-        Debug.log "Listen event call: #{target_element}:#{event.send(target_element)}"
-
-        unless time_valid?(event)
-          Debug.log 'Event happend in before start time'
-          return
-        end
-
-        target = event.send(target_element)
-        unless target
-          Debug.log 'Target is nil'
-          return
-        end
-
-        event.matches = target.match pattern
-        unless event.matches
-          Debug.log "Target is not match pattern(#{pattern})"
-          return
-        end
-
-        Debug.log "Callback event call: #{pattern}"
-        @callback.call(event)
+      def pattern=(pattern)
+        @pattern = pattern
       end
 
       def time_valid?(event)
         event.ts > Slappy.client.start_time
       end
 
-      private
+      def valid?(event)
+        unless time_valid?(event)
+          Debug.log 'Event happend in before start time'
+          return false
+        end
+
+        target = event.send(target_element)
+        unless target
+          Debug.log 'Target is nil'
+          return false
+        end
+
+        event.matches = target.match pattern
+        unless event.matches
+          Debug.log "Target is not match pattern(#{pattern})"
+          return false
+        end
+
+        true
+      end
 
       def target_element
         self.class.name.split('::').last.gsub(/Listener$/, '').underscore.to_sym
