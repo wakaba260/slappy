81f257c4221afba2c45f32c5996251c29fd97268
wakaba260 <moonoverwalker@gmail.com>
2015-11-22 00:40:39 +0900

    Added SlackAPI::Base#==

--- lib/slappy/slack_api/base.rb
+++ lib/slappy/slack_api/base.rb
@@ -2,6 +2,11 @@ module Slappy
   module SlackAPI
     class Base
       include Findable
+
+      def ==(other)
+        return false unless other.instance_of? self.class
+        other.id == id
+      end
     end
   end
 end
